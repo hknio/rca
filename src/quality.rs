800e096e3f10047915aadf99c1dd5a82b7bfbbe5
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2023-01-13 11:42:50 +0100

    add code coverage and dependency graph modules

--- src/quality.rs
+++ src/quality.rs
@@ -5,13 +5,39 @@ use std::process::Command;
 pub fn search(path: &str) {
     println!("{}", Green.bold().paint("# Number of SLOC:"));
     search_nb_sloc(path);
-    println!("\n")
+    println!("\n");
+    println!("{}", Green.bold().paint("# Dependency graph:"));
+    search_dependency_graph(path);
+    println!("\n");
+    println!("{}", Green.bold().paint("# Code coverage:"));
+    search_code_coverage(path);
+    println!("\n");
 }
 
 fn search_nb_sloc(path: &str) {
     let output = Command::new("tokei")
         //.current_dir(path)
         .args([path])
+        .output()
+        .expect("failed to execute process");
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
+}
+
+fn search_dependency_graph(path: &str) {
+    let output = Command::new("cargo")
+        .current_dir(path)
+        .args(["tree"])
+        .output()
+        .expect("failed to execute process");
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
+}
+
+fn search_code_coverage(path: &str) {
+    let output = Command::new("cargo")
+        .current_dir(path)
+        .args(["tarpaulin"])
         .output()
         .expect("failed to execute process");
     io::stdout().write_all(&output.stdout).unwrap();
